df8be4a6ca5467562aa7b3552a009e908a38aaf4
arslan759 <arslan.obaid759@gmail.com>
2022-12-29 07:23:55 +0500

    added project files

---
+++ src/Accordion.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Accordion from "react-bootstrap/Accordion";
+
+const Accordionn = ({ question }) => {
+  return (
+    <Accordion>
+      {question.map((item) => {
+        return (
+          <Accordion.Item
+            className="mb-3 shadow-sm"
+            key={item.id}
+            eventKey={item.id}
+          >
+            <Accordion.Header>{item.title}</Accordion.Header>
+            <Accordion.Body>{item.info}</Accordion.Body>
+          </Accordion.Item>
+        );
+      })}
+    </Accordion>
+  );
+};
+
+export default Accordionn;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Accordionn from "./Accordion";
+import questions from "./data";
 
 function App() {
+  const [question, setQuestion] = useState(questions);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main
+      style={{ height: "100vh" }}
+      className="d-flex justify-content-center align-items-center"
+    >
+      <section className="bg-white w-md-50 px-4 py-3">
+        <div className="d-md-flex">
+          <div className="col-md-3">
+            <h4>Questions And Answers About Login</h4>
+          </div>
+          <div className="col-md-9">
+            <Accordionn question={question} />
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
 
